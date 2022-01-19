9b4c402f238f4e112e92a395171d63a1ebfe8eab
Abdulraheem Sherif Adavuruku <63051314+SherifAdavuruku@users.noreply.github.com>
2022-01-19 18:17:52 +0100

    some change

--- src/stopwatch/StopWatch.js
+++ src/stopwatch/StopWatch.js
@@ -23,6 +23,8 @@ React.useEffect(() => {
 	};
 }, [isActive, isPaused]);
 
+// We know that componentWillUnmount executes when component is removed from actual DOM. Similarly if we use useEffect with an empty second argument and adding a return function call it will work as componentWillUnmount’
+
 const handleStart = () => {
 	setIsActive(true);
 	setIsPaused(false);
